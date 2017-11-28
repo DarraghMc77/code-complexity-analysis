d01aae4ccacacc86c12516dcc3b11b9d20d575d6
DarraghMc77 <mccaffrd@tcd.ie>
2017-11-28 22:19:05 +0000

    Added manager with endpoint returning commits for a repository

--- Manager/manager.py
+++ Manager/manager.py
@@ -1,6 +1,12 @@
-import config
 from pygit2 import Repository, clone_repository, GitError
 from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
+from flask import Flask
+
+GIT_REPO_URL = "https://github.com/DarraghMc77/chat-server.git"
+GIT_REPO_PATH = "./repo"
+commit_list = []
+
+app = Flask(__name__)
 
 def get_git_commits(repo):
     commits = []
@@ -9,17 +15,26 @@ def get_git_commits(repo):
     return commits
 
 def clone_git_repository():
-    repo_url = config.GIT_REPO_URL
-    repo_path = config.GIT_REPO_PATH
+    repo_url = GIT_REPO_URL
+    repo_path = GIT_REPO_PATH
     try:
         repo = Repository(repo_path + '/.git')
     except GitError as e:
         repo = clone_repository(repo_url, repo_path)
+        print(repo.head)
     return repo
+
+@app.route("/get_task")
+def hello():
+    global commit_list
+    print(commit_list)
+    return commit_list[0]
 
 def main():
     repo = clone_git_repository()
-    print(get_git_commits(repo))
+    commit_list = get_git_commits(repo)
+    print(commit_list)
+    app.run(host='0.0.0.0', port=80)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- config.py
+++
@@ -1,2 +0,0 @@
-GIT_REPO_URL = "https://github.com/DarraghMc77/chat-server.git"
-GIT_REPO_PATH = "/Users/Darragh/College/ScalableComputing/code-complexity-analysis/chat-server"
