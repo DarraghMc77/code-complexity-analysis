f1828d9d6ead0ced54502a13a1108c91b0f0cdee
DarraghMc77 <mccaffrd@tcd.ie>
2017-12-03 17:00:04 +0000

    Calculating time taken to complete work

--- manager/manager.py
+++ manager/manager.py
@@ -7,12 +7,15 @@ import json
 import sys
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import time
 
 GIT_REPO_URL = "https://github.com/KupynOrest/DeblurGAN.git"
 GIT_REPO_PATH = "./repo"
 commit_list = []
 complexity_results = []
 index = 0
+start_time = time.time()
+first_request = True
 
 app = Flask(__name__)
 
@@ -55,13 +58,30 @@ def calculate_complexity():
     print(index, file=sys.stderr)
     print(complexity, file=sys.stderr)
     complexity_results[index] = complexity
-    if(len(complexity_results) >= len(commit_list)):
+    if(complexity_results[len(complexity_results)-1] is not 0):
+        print("END TIME", file=sys.stderr)
+        end_time = time.time()
+        print(end_time, file=sys.stderr)
+        print("TIME TAKEN", file=sys.stderr)
+        time_taken = start_time - end_time
+        print(time_taken, file=sys.stderr)
         graph_results(complexity_results)
     print(complexity_results, file=sys.stderr)
-    return request, 200
+    return "Results Received", 200
 
 @app.route("/get_task")
 def send_tasks():
+    # Start timer when first GET request is made
+    global start_time
+    global first_request
+
+    if(first_request):
+        start_time = time.time()
+        print("STARTING TIME", file=sys.stderr)
+        print(start_time, file=sys.stderr)
+
+    first_request = False
+
     global commit_list
     global index
     if index >= len(commit_list):
